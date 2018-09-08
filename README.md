e38a5bc0804c7e340f1f26771cbf0c4069905540
Luke Thompson <luke@streamjar.tv>
2018-09-08 03:57:00 +0100

    fix: export platforms correctly

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # @streamjar/ui
 The UI library powering StreamJar 🚀
 
+Oh no?! where has the history gone I hear you say?  The old UI with it's history is archived [here](https://github.com/StreamJar/ui-old)
+
+
 // todo: docs
--- packages/@streamjar/ui-ng/src/lib/platforms/index.ts
+++ packages/@streamjar/ui-ng/src/lib/platforms/index.ts
@@ -1,9 +1,9 @@
 import { FormsModule } from '@angular/forms';
-import { CheckboxModule } from 'index';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { CheckboxModule } from '../checkbox';
 import { PlatformsComponent } from './platforms.component';
 
 @NgModule({
