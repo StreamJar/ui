81cf2da7ce304fdb622d7aecf91cdad49b9684d3
Ethan Waite <ethan@streamjar.tv>
2019-07-25 18:02:03 +0100

    feat(tabs): add draggable and deselectable tabs (#14)
    * feat(tabs): add draggable tabs
    
    * feat(tabs): address feedback and add deselectable tabs

--- packages/@streamjar/ui-react/demo/src/components/tabs-demo.tsx
+++ packages/@streamjar/ui-react/demo/src/components/tabs-demo.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Tab, Tabs } from '../../../src/lib';
 import { Demo, IDemoConfig } from '../demo/demo';
 
-export class TabsDemo extends React.Component {
+export class TabsDemo extends React.Component<{}, { value: string; tabs: string[] }> {
 	public config: IDemoConfig = {
 		name: 'Tabs',
 		components: [{
@@ -11,13 +11,29 @@ export class TabsDemo extends React.Component {
 			props: [{
 				name: 'value',
 				default: '',
-				description: 'The current tab selection. Defaults to the first tab',
-				type: 'string | boolean | number',
+				description: 'The current tab selection. Defaults to the first tab, and may be null if tabs are deselectable',
+				type: 'string | boolean | number | null',
+			}, {
+				name: 'draggable',
+				default: 'false',
+				description: 'Whether tabs within the element can be repositioned by the user by dragging each one',
+				type: 'boolean',
+			}, {
+				name: 'deselectable',
+				default: 'false',
+				description: 'Whether an active tab may be clicked again to set the value to null',
+				type: 'boolean',
 			}, {
 				name: 'onChange',
 				default: '() => { }',
-				type: '(value: string | boolean | number) => void',
+				type: '(value: string | boolean | number | null) => void',
 				description: 'Callback for when the tab is changed',
+			},
+			{
+				name: 'onMove',
+				default: '() => { }',
+				type: '(key: string, previousIndex: number, nextIndex: number) => void',
+				description: 'Callback for when a tab has been moved to a new position',
 			}],
 		}, {
 			name: 'Tab',
@@ -31,24 +47,65 @@ export class TabsDemo extends React.Component {
 				default: 'false',
 				description: 'Whether the tab is disabled',
 				type: 'boolean',
+			}, {
+				name: 'draggable',
+				default: 'false',
+				description: 'Whether the tab can be dragged by the user',
+				type: 'boolean',
+			}, {
+				name: 'index',
+				default: '',
+				description: 'If this tab is draggable, this index indicates the current placement of the tab',
+				type: 'number',
 			}],
 		}],
 		examples: `
-<Tabs value={true}>
-	<Tab value={true}>yes</Tab>
-	<Tab value={false}>no</Tab>
-</Tabs>
-<Tabs value={true}>
-	<Tab value={true}>yes</Tab>
-	<Tab disabled={true} value={false}>no</Tab>
-</Tabs>
+constructor(props) {
+	super(props);
+
+	this.state = { value: 'Tips', tabs: ['Tips', 'Followers', 'Subscribers', 'Hosts'] };
+}
+
+public render() {
+	return (
+		<Demo config={this.config}>
+			<Tabs>
+				<Tab value={true}>yes</Tab>
+				<Tab value={false}>no</Tab>
+			</Tabs>
+			<Tabs value={true}>
+				<Tab value={true}>yes</Tab>
+				<Tab disabled={true} value={false}>no</Tab>
+			</Tabs>
+			<Tabs value={this.state.value} draggable={true} deselectable={true} onChange={this.handleChange} onMove={this.handleMove}>
+				{this.state.tabs.map((value, index) => <Tab key={value} value={value} draggable={true} index={index}>{value}</Tab>)}
+			</Tabs>
+		</Demo>
+	);
+}
+
+private readonly handleChange = (value: string) => {
+	this.setState({ value });
+}
+
+private readonly handleMove = (_: string, previousIndex: number, nextIndex: number) => {
+	const tabs = [...this.state.tabs];
+	tabs.splice(nextIndex, 0, tabs.splice(previousIndex, 1)[0]);
+	this.setState({ tabs });
+}
 		`,
 	};
+
+	constructor(props) {
+		super(props);
+
+		this.state = { value: 'Tips', tabs: ['Tips', 'Followers', 'Subscribers', 'Hosts'] };
+	}
 
 	public render() {
 		return (
 			<Demo config={this.config}>
-				<Tabs value={true}>
+				<Tabs>
 					<Tab value={true}>yes</Tab>
 					<Tab value={false}>no</Tab>
 				</Tabs>
@@ -56,7 +113,20 @@ export class TabsDemo extends React.Component {
 					<Tab value={true}>yes</Tab>
 					<Tab disabled={true} value={false}>no</Tab>
 				</Tabs>
+				<Tabs value={this.state.value} draggable={true} deselectable={true} onChange={this.handleChange} onMove={this.handleMove}>
+					{this.state.tabs.map((value, index) => <Tab key={value} value={value} draggable={true} index={index}>{value}</Tab>)}
+				</Tabs>
 			</Demo>
 		);
+	}
+
+	private readonly handleChange = (value: string) => {
+		this.setState({ value });
+	}
+
+	private readonly handleMove = (_: string, previousIndex: number, nextIndex: number) => {
+		const tabs = [...this.state.tabs];
+		tabs.splice(nextIndex, 0, tabs.splice(previousIndex, 1)[0]);
+		this.setState({ tabs });
 	}
 }
